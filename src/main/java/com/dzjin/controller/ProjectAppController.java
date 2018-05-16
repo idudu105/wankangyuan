76c23979143c9b9998bf3e1c2ce9950774fd0d1c
dzjin5678 <dzjin5678@163.com>
2018-05-16 13:24:00 +0800

    完善项目文件增删查改功能
    完善项目文件增删查改功能

---
+++ src/main/java/com/dzjin/controller/ProjectAppController.java
@@ -0,0 +1,24 @@
+package com.dzjin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 项目-应用相关操作接口
+ * 项目名称：wankangyuan 
+ * 类名称：ProjectAppController 
+ * 类描述： 
+ * 创建人：dzjin 
+ * 创建时间：2018年5月16日 上午10:30:04 
+ * 修改人：dzjin 
+ * 修改时间：2018年5月16日 上午10:30:04 
+ * 修改备注： 
+ * @version 
+ *
+ */
+
+@Controller
+@RequestMapping("/projectApp")
+public class ProjectAppController {
+
+}
